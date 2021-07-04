e2e80884b7e36df8a7cc1dee981eaa92ff2340d0
ubaideforte <ubaid@eforte.net>
2021-07-04 02:00:23 -0700

    add some content in order page

--- pages/order.jsx
+++ pages/order.jsx
@@ -6,9 +6,9 @@ const Order = () => {
   return (
     <>
       <div className="col-9 mx-auto my-4 fw-bold">Home / Cards / Order</div>
-      <Row className="col-9 mx-auto ">
+      <Row className="col-9 mx-auto position-relative">
         <Col span={12}>
-          <div style={{ width: 400 }}>
+          <div style={{ width: 400, position: "sticky", top: 60 }}>
             <Carousel infiniteLoop autoPlay>
               <div>
                 <img alt="card-image" src={cardImage1} className="img-fluid" />
@@ -29,17 +29,77 @@ const Order = () => {
           </div>
         </Col>
 
-        <Col span={12}>
+        <Col span={12} style={{ height: "180vh" }}>
           <div
             className="order"
             style={{
               width: "-webkit-fill-available",
-              border: "1px solid black",
-              height: 250,
+              border: "1px solid #acacac",
+              backgroundColor: "#F5F5F5",
+              padding: 16,
             }}
-          ></div>
+          >
+            <p>
+              Park Avenue shirts are ideal for premium office wear. Personalise your
+              shirts with your text or logo and create a premium identity for your brand.
+            </p>
+
+            <ul>
+              <li>
+                <p>
+                  <b>Brand:</b> Park Avenue
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Material:</b> 100 % cotton woven fabric.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Gender:</b> Men
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Chest Pocket:</b> Left
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Sleeves:</b> Full-sleeves
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Country of Origin:</b> Pakistan
+                </p>
+              </li>
+            </ul>
+
+            <p>CustomizationTechnology: Embriodery</p>
+
+            <p>Available Colors:</p>
+            <div style={{ backgroundColor: "#fce56a", height: 24, width: 24 }} />
+
+            <p>Size Available:</p>
+            <div>32 34 36 38</div>
+
+            <div
+              style={{
+                backgroundColor: "#bcde34",
+                width: 240,
+                padding: "12px 0",
+                cursor: "pointer",
+                textAlign: "center",
+              }}
+            >
+              Get Started
+            </div>
+          </div>
         </Col>
       </Row>
+      <div style={{ height: 200 }}></div>
     </>
   );
 };
